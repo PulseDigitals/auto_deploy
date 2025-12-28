d50733a726d947fe8b4384216ce422abb0f1bc21
Gbadebo Ologbenla <isholla360@gmail.com>
2025-12-28 07:24:57 +0100

    Deploy current build

--- convex/http.ts
+++ convex/http.ts
@@ -1,7 +1,14 @@
 import { httpRouter, type PublicHttpAction } from "convex/server";
+import { startVercelOAuth } from "./oauth/vercel";
 import { handleCentralCallback } from "./gateway.node";
 
 const http = httpRouter();
+
+http.route({
+  path: "/auth/vercel/start",
+  method: "GET",
+  handler: startVercelOAuth,
+});
 
 http.route({
   path: "/oauth/callback",
